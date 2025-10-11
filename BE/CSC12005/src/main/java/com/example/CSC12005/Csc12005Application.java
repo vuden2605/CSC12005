f9e1ab4c7fb0e2352e1f8657853f65937f954594
thanhan06 <nguyenvanvu060104@gmail.com>
2025-10-11 23:24:50 +0700

    setup fe and be

---
+++ BE/CSC12005/src/main/java/com/example/CSC12005/Csc12005Application.java
@@ -0,0 +1,13 @@
+package com.example.CSC12005;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Csc12005Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Csc12005Application.class, args);
+	}
+
+}
---
+++ BE/CSC12005/src/test/java/com/example/CSC12005/Csc12005ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CSC12005;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Csc12005ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
