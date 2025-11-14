d946210bbd026d326f267029733cf5f22d01423a
thanhan06 <nguyenvanvu060104@gmail.com>
2025-11-14 21:59:16 +0700

    UCMN01_VV

--- FE/CSC12005/src/routes/index.jsx
+++ FE/CSC12005/src/routes/index.jsx
@@ -10,7 +10,7 @@ import { Information } from "../pages/Employee/Darshboard/Information";
 import { PersonalInfo } from "../pages/Employee/Darshboard/Information/PersonalInfo";
 import { InfoDetails } from "../pages/Employee/Darshboard/Information/InfoDetails/index";
 import { SalaryInfo } from "../pages/Employee/Darshboard/Information/SalaryInfo";
-import {Requests} from "../pages/Employee/Darshboard/Requests"
+import { Requests } from "../pages/Employee/Darshboard/Requests"
 export const routes = [
   {
     path: "/",
@@ -20,30 +20,34 @@ export const routes = [
       { path: "login", element: <Login /> },
       { path: "admin", element: <Admin /> },
       { path: "manager", element: <Manager /> },
-      { path: "employee", 
+      {
+        path: "employee",
         element: <Employee />,
         children: [
-          { path: "dashboard", 
-              element: <Dashboard/>,
-              children: [
-                { index: true, element: <Information /> },
-                { path: "info", element: <Information />,
-                  children: [
-                    {index: true, element: <PersonalInfo />},
-                    { path: "personal-info", element: <PersonalInfo /> },
-                    { path: "info-details", element: <InfoDetails /> },
-                    {path: "salary-info", element:<SalaryInfo/>}
-                  ]
-                },
-                // { path: "attendance", element: <Attendance /> },
-                { path: "request", element: <Requests/>},
-                // { path: "event", element: <Event /> },
-                // { path: "score", element: <Score /> },
-              ]
+          {
+            path: "dashboard",
+            element: <Dashboard />,
+            children: [
+              { index: true, element: <Information /> },
+              {
+                path: "info", element: <Information />,
+                children: [
+                  { index: true, element: <PersonalInfo /> },
+                  { path: "personal-info", element: <PersonalInfo /> },
+                  { path: "info-details", element: <InfoDetails /> },
+                  { path: "salary-info", element: <SalaryInfo /> }
+                ]
+              },
+              // { path: "attendance", element: <Attendance /> },
+              { path: "request", element: <Requests /> },
+              // { path: "event", element: <Event /> },
+              // { path: "score", element: <Score /> },
+            ]
           },
         ]
-       },
+      },
       { path: "hr", element: <HRAdmin /> },
+      { path: "manager", element: <Manager /> }
     ],
   },
 ];
