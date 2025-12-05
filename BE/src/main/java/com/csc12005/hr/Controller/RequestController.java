3dcc1029837d566d6f220a257dd10b7323a1eec5
vuden2605 <vuden2605@gmail.com>
2025-12-05 22:40:37 +0700

    fix request service

--- BE/src/main/java/com/csc12005/hr/Controller/RequestController.java
+++ BE/src/main/java/com/csc12005/hr/Controller/RequestController.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -28,8 +29,8 @@ public class RequestController {
 	}
 	@GetMapping("/requests/me")
 	public ApiResponse<Page<RequestResponse>> myRequests (
-			PageRequestDTO pageRequest,
-			RequestFilter requestFilter) {
+			@ModelAttribute PageRequestDTO pageRequest,
+			@ModelAttribute RequestFilter requestFilter) {
 		return ApiResponse.<Page<RequestResponse>>builder()
 				.message("Get my request success")
 				.data(requestService.myRequests(pageRequest, requestFilter))
