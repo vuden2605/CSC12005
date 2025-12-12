a7cbee94f207a768f5be91eb904b86e423092442
vuden2605 <vuden2605@gmail.com>
2025-12-12 11:23:26 +0700

    add constructor for pagerequestdto

--- BE/src/main/java/com/csc12005/hr/DTO/Request/PageRequestDTO.java
+++ BE/src/main/java/com/csc12005/hr/DTO/Request/PageRequestDTO.java
@@ -1,17 +1,26 @@
 package com.csc12005.hr.DTO.Request;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class PageRequestDTO {
+	@Builder.Default
 	private Integer page = 0;
+	@Builder.Default
 	private Integer size = 10;
+	@Builder.Default
 	private String sortBy = "id";
+	@Builder.Default
 	private Sort.Direction direction = Sort.Direction.ASC;
 	public Pageable buildPageable() {
 		return PageRequest.of(
